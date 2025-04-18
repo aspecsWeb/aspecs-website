17cdfb11f9c13c7e496a866859407a9ff32d480d
Paul Pietzko <paul.pietzko@icloud.com>
2025-04-18 20:36:53 +0200

    feat: implement dynamic social links in Contact component

---
+++ src/lib/sociallinks.ts
@@ -0,0 +1,22 @@
+export const socialLinks = [
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/aspecsweb/",
+    iconClass: "ri-instagram-fill",
+  },
+  {
+    name: "Twitter",
+    href: "https://x.com/aspecsweb",
+    iconClass: "ri-twitter-fill",
+  },
+  {
+    name: "YouTube",
+    href: "#",
+    iconClass: "ri-youtube-fill",
+  },
+  {
+    name: "Email",
+    href: "mailto:info@aspecs.ch",
+    iconClass: "ri-mail-fill",
+  },
+];
